6b4df42e8aa1961b46aa55f230dfd9a3fa52f88e
Christian Soler <chr.solr@gmail.com>
2022-10-05 18:32:15 -0500

    added a eslint rule

--- src/hooks/useRandomQuote.tsx
+++ src/hooks/useRandomQuote.tsx
@@ -23,6 +23,7 @@ export default function useRandomQuote(): HookProps {
     if (!quote) {
       ;(async () => await getQuote())()
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [quote])
 
   return { quote, getQuote }
