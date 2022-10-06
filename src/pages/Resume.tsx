2bf8655c48223b7f669bf494fd54d8de8d51658e
chrsolr <christian@innowatts.com>
2022-10-06 09:13:36 -0500

    WIP

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import PageWrapper from '../shared/components/PageWrapper'
 import Typography from '../shared/components/Typography'
+import Image from '../shared/components/Image'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const SocialIconsWrapper = styled.div`
+  display: flex;
+  a {
+    margin: 1rem
+  }
+`
 
 export default function Resume() {
+  const avatarUrl = 'https://media-exp1.licdn.com/dms/image/C5603AQGKKZ_eir17GA/profile-displayphoto-shrink_200_200/0/1516804819724?e=1670457600&v=beta&t=DT2Sp8KTwT_uAHQ9l89ZojXUbsxKS_vyk6aaoqNQwLU'
   return (
     <PageWrapper>
-      <img alt='' src="https://media-exp1.licdn.com/dms/image/C5603AQGKKZ_eir17GA/profile-displayphoto-shrink_200_200/0/1516804819724?e=1670457600&v=beta&t=DT2Sp8KTwT_uAHQ9l89ZojXUbsxKS_vyk6aaoqNQwLU" />
-      <Typography>chr.solr@gmail.com • github.com/chrsolr</Typography>
+      <ContentWrapper>
+        <Image src={avatarUrl} isRounded />
+        <SocialIconsWrapper>
+          <a href='https://github.com/chrsolr' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faGithub} size='2x' />
+          </a>
+          <a href='https://www.linkedin.com/in/christiansoler/' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faLinkedin} size='2x' />
+          </a>
+        </SocialIconsWrapper>
+      </ContentWrapper>
+
+
       <Typography>
         As a passionate person, I pride myself on every project I take on. I am
         self-motivated and driven by puzzles and challenges. As a developer, I
--- src/shared/components/Image.tsx
+++ src/shared/components/Image.tsx
@@ -3,16 +3,18 @@ import styled from 'styled-components'
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
   isResponsive?: boolean
+  isRounded?: boolean
 }
 
 const StyledImage = styled.img<{
   isResponsive?: boolean
+  isRounded?: boolean
 }>`
   width: ${(props) => (props.isResponsive ? '100%' : null)};
-  border-radius: ${(props) => props.theme.dimensions.borderRadius};
+  border-radius: ${(props) => props.isRounded ? '50%' : props.theme.dimensions.borderRadius};
   margin-bottom: 1rem;
 `
 
-export default function Image({ isResponsive, ...props }: Props) {
-  return <StyledImage isResponsive={isResponsive} {...props} />
+export default function Image({ isResponsive, isRounded, ...props }: Props) {
+  return <StyledImage isResponsive={isResponsive} isRounded={isRounded} {...props} />
 }
