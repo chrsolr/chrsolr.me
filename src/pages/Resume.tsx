00fd5d2e5ef6d8fab550670e702846e0e8fe3bc4
Christian Soler <chr.solr@gmail.com>
2022-10-05 22:59:14 -0500

    WIP: Adding raw text

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import PageWrapper from '../shared/components/PageWrapper'
+import Typography from '../shared/components/Typography'
 
 export default function Resume() {
   return (
     <PageWrapper>
-      <p>RESUME</p>
+      <img src="https://media-exp1.licdn.com/dms/image/C5603AQGKKZ_eir17GA/profile-displayphoto-shrink_200_200/0/1516804819724?e=1670457600&v=beta&t=DT2Sp8KTwT_uAHQ9l89ZojXUbsxKS_vyk6aaoqNQwLU" />
+      <Typography>chr.solr@gmail.com • github.com/chrsolr</Typography>
+      <Typography>
+        As a passionate person, I pride myself on every project I take on. I am
+        self-motivated and driven by puzzles and challenges. As a developer, I
+        enjoy working in a team environment. In addition, I thrive under
+        pressure when working on multiple projects.
+      </Typography>
+      <Typography>
+        I am an experienced Full Stack Web Developer, as well as a Mobile
+        Developer Hobbyist. I am looking for an opportunity to work in a company
+        with a great environment, where I can expand my knowledge and help a
+        company with my experience. As well as becoming part of a team that is
+        as passionate about programming as I am.
+      </Typography>
+
+      <Typography>
+        JavaScript, AngularJS, C#, JAVA, MeteorJS, Kubernetes, Docker,
+        RequireJs, NodeJS, JQuery, TypeScript, Entity Framework, ASP.NET, JSON,
+        HTTP, Android, Bootstrap, LESS/SASS, LINQ, Github, TFS, Bitbucket.
+      </Typography>
     </PageWrapper>
   )
 }
