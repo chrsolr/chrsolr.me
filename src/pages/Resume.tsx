0c92440d9f39a7780b82f80907a7c0701c510fca
Christian Soler <chr.solr@gmail.com>
2022-10-06 19:56:49 -0500

    WIP: Resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,8 +4,7 @@ import Typography from '../shared/components/Typography'
 import Image from '../shared/components/Image'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
+import resumeData from '../data/resume'
 
 const ContentWrapper = styled.div`
   display: flex;
@@ -16,47 +15,33 @@ const ContentWrapper = styled.div`
 
 const SocialIconsWrapper = styled.div`
   display: flex;
+
   a {
-    margin: 1rem
+    margin: 1rem;
   }
 `
 
 export default function Resume() {
-  const avatarUrl = 'https://media-exp1.licdn.com/dms/image/C5603AQGKKZ_eir17GA/profile-displayphoto-shrink_200_200/0/1516804819724?e=1670457600&v=beta&t=DT2Sp8KTwT_uAHQ9l89ZojXUbsxKS_vyk6aaoqNQwLU'
+  const avatarUrl = resumeData.profileImageUrl
   return (
     <PageWrapper>
       <ContentWrapper>
         <Image src={avatarUrl} isRounded />
         <SocialIconsWrapper>
-          <a href='https://github.com/chrsolr' target='_blank' rel='noreferrer'>
-            <FontAwesomeIcon icon={faGithub} size='2x' />
-          </a>
-          <a href='https://www.linkedin.com/in/christiansoler/' target='_blank' rel='noreferrer'>
-            <FontAwesomeIcon icon={faLinkedin} size='2x' />
-          </a>
+          {Boolean(resumeData.socials.length) &&
+            resumeData.socials.map((social) => (
+              <a href={social.url} target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={social.icon} size="2x" />
+              </a>
+            ))}
         </SocialIconsWrapper>
       </ContentWrapper>
 
+      {Boolean(resumeData.aboutMe.length) &&
+        resumeData.aboutMe.map((about) => <Typography>{about}</Typography>)}
 
-      <Typography>
-        As a passionate person, I pride myself on every project I take on. I am
-        self-motivated and driven by puzzles and challenges. As a developer, I
-        enjoy working in a team environment. In addition, I thrive under
-        pressure when working on multiple projects.
-      </Typography>
-      <Typography>
-        I am an experienced Full Stack Web Developer, as well as a Mobile
-        Developer Hobbyist. I am looking for an opportunity to work in a company
-        with a great environment, where I can expand my knowledge and help a
-        company with my experience. As well as becoming part of a team that is
-        as passionate about programming as I am.
-      </Typography>
-
-      <Typography>
-        JavaScript, AngularJS, C#, JAVA, MeteorJS, Kubernetes, Docker,
-        RequireJs, NodeJS, JQuery, TypeScript, Entity Framework, ASP.NET, JSON,
-        HTTP, Android, Bootstrap, LESS/SASS, LINQ, Github, TFS, Bitbucket.
-      </Typography>
+      {Boolean(resumeData.skills.length) &&
+        resumeData.skills.map((skill) => <Typography>{skill}</Typography>)}
     </PageWrapper>
   )
 }
