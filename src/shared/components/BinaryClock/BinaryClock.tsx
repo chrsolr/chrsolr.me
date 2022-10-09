d84044920e92d4fae88ace0ff4fe5e132978c7c6
Christian Soler <chr.solr@gmail.com>
2022-10-09 01:21:23 -0500

    WIP: Cleaned Up Binary Clock

--- src/shared/components/BinaryClock/BinaryClock.tsx
+++ src/shared/components/BinaryClock/BinaryClock.tsx
@@ -2,9 +2,21 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import Typography from '../Typography'
 
+type BinaryClockTime = {
+  hours: readonly [number, number]
+  minutes: readonly [number, number]
+  seconds: readonly [number, number]
+}
+
 interface Props {
   activeIndexes?: number
 }
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const ContentWrapper = styled.div`
   display: flex;
@@ -14,7 +26,7 @@ const ContentWrapper = styled.div`
     margin-right: 1rem;
   }
 `
-const Circle = styled.div<{ isActive?: boolean }>`
+const Pad = styled.div<{ isActive?: boolean }>`
   background-color: ${(props) =>
     props.isActive ? props.theme.colors.accent : props.theme.colors.foreground};
   border-radius: ${(props) => props.theme.dimensions.borderRadius};
@@ -23,66 +35,78 @@ const Circle = styled.div<{ isActive?: boolean }>`
   margin: 0.5rem;
 `
 
-function digitsToArray(digit: number) {
-  const digits = digit
+/**
+ * Create Current Time String
+ */
+function getClockTimeLabel(timeAsDigits: BinaryClockTime) {
+  const hours = timeAsDigits.hours.join('')
+  const minutes = timeAsDigits.minutes.join('')
+  const seconds = timeAsDigits.seconds.join('')
+  return `${hours} : ${minutes} : ${seconds}`
+}
+
+/**
+ * Split an hour, minute or second to a tuple
+ * e.g. 15 -> [1,5] || 5 -> [0,5]
+ */
+function splitDigitToTuple(digit: number): [number, number] {
+  const split = digit
     .toString()
     .split('')
     .map((n) => +n)
-  return digits.length > 1 ? digits : [0, ...digits]
+  const isSingleDigit = split.length === 1
+  return [isSingleDigit ? 0 : split[0], isSingleDigit ? split[0] : split[1]]
 }
 
+// TODO: CLEAN-UP
 function BinaryClockGroup({ activeIndexes }: Props) {
-  const actives = [false, false, false, false]
+  const defaultState = [false, false, false, false]
   activeIndexes
     ?.toString(2)
     .split('')
     .reverse()
     .forEach((v, i) => {
-      actives[i] = v !== '0'
+      defaultState[i] = v !== '0'
     })
-  console.log('activeIndexes', activeIndexes, actives)
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {actives.reverse().map((value) => (
-        <Circle isActive={value} />
+      {defaultState.reverse().map((value) => (
+        <Pad isActive={value} />
       ))}
     </div>
   )
 }
 
 export default function BinaryClock() {
-  const [time, setTime] = useState({ hours: [], minutes: [], seconds: [] })
+  const [timeAsDigits, setTimeAsDigits] = useState<BinaryClockTime>({
+    hours: [0, 0],
+    minutes: [0, 0],
+    seconds: [0, 0],
+  })
 
   setInterval(() => {
     const date = new Date()
     const currentTime = {
-      hours: digitsToArray(date.getHours()),
-      minutes: digitsToArray(date.getMinutes()),
-      seconds: digitsToArray(date.getSeconds()),
+      hours: splitDigitToTuple(date.getHours()),
+      minutes: splitDigitToTuple(date.getMinutes()),
+      seconds: splitDigitToTuple(date.getSeconds()),
     }
-    setTime(currentTime as any)
+    setTimeAsDigits(currentTime)
   }, 1000)
 
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
+    <MainContent>
       <ContentWrapper>
-        <BinaryClockGroup activeIndexes={time.hours[0]} />
-        <BinaryClockGroup activeIndexes={time.hours[1]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.hours[0]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.hours[1]} />
 
-        <BinaryClockGroup activeIndexes={time.minutes[0]} />
-        <BinaryClockGroup activeIndexes={time.minutes[1]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.minutes[0]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.minutes[1]} />
 
-        <BinaryClockGroup activeIndexes={time.seconds[0]} />
-        <BinaryClockGroup activeIndexes={time.seconds[1]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.seconds[0]} />
+        <BinaryClockGroup activeIndexes={timeAsDigits.seconds[1]} />
       </ContentWrapper>
-      <Typography size="xxl">
-        {time.hours[0]}
-        {time.hours[1]} : {time.minutes[0]}
-        {time.minutes[1]} : {time.seconds[0]}
-        {time.seconds[1]}
-      </Typography>
-    </div>
+      <Typography size="xxl">{getClockTimeLabel(timeAsDigits)}</Typography>
+    </MainContent>
   )
 }
