1fa39d66886f68c247765e4fdc202b9b041d1cbf
chrsolr <chr.solr@gmail.com>
2023-03-24 14:20:11 -0500

    Added seed file

--- src/server/api/routers/blog-posts.ts
+++ src/server/api/routers/blog-posts.ts
@@ -6,12 +6,12 @@ export const blogPostRouter = createTRPCRouter({
   getById: publicProcedure
     .input(z.object({ blogPostId: z.string() }))
     .query(({ input, ctx }) => {
-      return ctx.prisma.blogPost.findUnique({
+      return ctx.prisma.blogPosts.findUnique({
         where: { id: input.blogPostId },
       });
     }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.blogPost.findMany({
+    return ctx.prisma.blogPosts.findMany({
       where: { active: true },
       orderBy: { createdAt: "desc" },
     });
