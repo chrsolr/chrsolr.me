77b509d83cb33190e069f96e562151e6095fa818
chrsolr <chr.solr@gmail.com>
2023-03-25 11:05:54 -0500

    Updated lints and some initial tailwinds globals

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  semi: false,
+  trailingComma: "all",
+  singleQuote: true,
+}
 
-module.exports = config;
+module.exports = config
