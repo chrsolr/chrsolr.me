de65cead281da5d29402725a6e69e29ee02e253b
chrsolr <chr.solr@gmail.com>
2023-03-25 11:05:54 -0500

    Updated lints and some initial tailwinds globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
-import { type AppType } from "next/app";
+import { type AppType } from "next/app"
 
-import { api } from "~/utils/api";
+import { api } from "~/utils/api"
 
-import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import "~/styles/globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
+import React, { type ReactNode } from "react"
+import { TopNavigationBar } from "~/components/TopNavigationBar"
+
+function NavigationLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <TopNavigationBar />
+      {children}
+    </>
+  )
+}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
+      <NavigationLayout>
+        <Component {...pageProps} />
+      </NavigationLayout>
     </ClerkProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
