988c45b0a8ff14df951e4dac1b6c3a6b655ae8a7
chrsolr <chr.solr@gmail.com>
2023-03-25 22:45:20 -0500

    Working on Sidebar

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,9 @@ import { v4 as uuidv4 } from 'uuid'
 export function getUniqueKey(): string {
   return uuidv4()
 }
+
+export function mergeTailwindClasses(classes: string) {
+  return Array.from(
+    new Set(classes.split(' ').filter((v) => v !== 'undefined')),
+  ).join(' ')
+}
