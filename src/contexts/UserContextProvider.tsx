b68006061cd601c6684fbe3b455903c5a26836ea
chrsolr <chr.solr@gmail.com>
2023-03-26 13:50:49 -0500

    WIP: UserContext

--- src/contexts/UserContextProvider.tsx
+++ src/contexts/UserContextProvider.tsx
@@ -11,10 +11,10 @@ export const UserContext = React.createContext<Props>({ user: undefined })
 export const UserProvider = function ({ children }: { children: ReactNode }) {
   const user = useUser()
   const { data: currentUserData } = api.users.getById.useQuery({
-    userId: 'clfmxnryf00008nk0vsot29nj1',
-    id: user.user?.id,
+    userId: user.user?.id,
   })
   const dto = { ...currentUserData, ...user } as UserDTO
+
   return (
     <UserContext.Provider value={{ user: dto }}>
       {children}
