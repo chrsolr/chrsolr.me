acf9e7c70acc2b8a02dcf4511bfdf7c5ec26a3a5
chrsolr <chr.solr@gmail.com>
2023-03-26 20:10:32 -0500

    Added Home Page Quotes

---
+++ src/hooks/useRandomQuote.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { type QuoteProps } from '~/components/Quote'
+
+type HookReturnValue = {
+  quote?: QuoteProps
+  getQuote: () => Promise<void>
+  URL: string
+}
+
+export const useRandomQuote = (): HookReturnValue => {
+  const URL = 'https://api.quotable.io/random'
+  const [quote, setQuote] = useState<QuoteProps>()
+
+  const getQuote = async () => {
+    try {
+      const response = (await (await fetch(URL as string)).json()) as QuoteProps
+      setQuote(response)
+    } catch (error) {
+      throw new Error('Error: Fetching Quote')
+    }
+  }
+
+  useEffect(() => {
+    if (!quote) {
+      void getQuote()
+    }
+  }, [quote])
+
+  return { quote, getQuote, URL }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
 import { PageWrapper } from '~/components/PageWrapper'
+import { Quote } from '~/components/Quote'
+import { useRandomQuote } from '~/hooks/useRandomQuote'
 
 const Home: NextPage = () => {
+  const { quote, URL, getQuote } = useRandomQuote()
+
   return (
     <>
       <Head>
         <title>chrsolr.me - home</title>
       </Head>
-      <PageWrapper>
-        <h1 className="bg-red-600 text-center">HOME PAGE CONTENT</h1>
+      <PageWrapper disableContainer>
+        {quote && (
+          <div className="container mx-auto flex flex-1 flex-col justify-center">
+            <Quote
+              content={quote.content}
+              author={quote.author}
+              tags={quote.tags}
+              authorSlug={quote.authorSlug}
+              permalink={URL}
+            />
+          </div>
+        )}
       </PageWrapper>
     </>
   )
