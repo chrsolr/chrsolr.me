7a79180d5732b2266adc7f4bbbe0bd1dae2d4cbe
chrsolr <chr.solr@gmail.com>
2023-03-28 18:16:40 -0500

    WIP: Resume Page

---
+++ src/pages/resume.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { PageWrapper } from "~/components/PageWrapper";
+
+export default function Resume() {
+  return <>
+  <Head>
+        <title>chrsolr.me - home</title>
+      </Head>
+      <PageWrapper>
+        RESUME
+      </PageWrapper>
+  </>
+}
