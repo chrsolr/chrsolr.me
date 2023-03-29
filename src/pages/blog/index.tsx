77e10800975073b1c21aaac7ffdcc8f0ea8d65ca
chrsolr <chr.solr@gmail.com>
2023-03-28 21:54:40 -0500

    added blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,13 +1,45 @@
-import Head from "next/head";
-import { PageWrapper } from "~/components/PageWrapper";
+/* eslint-disable @next/next/no-img-element */
+import Head from 'next/head'
+import Link from 'next/link'
+import { PageWrapper } from '~/components/PageWrapper'
+import { Typography } from '~/components/Typography'
+import { api } from '~/utils/api'
+import { getUniqueKey } from '~/utils/helpers'
 
 export default function Blog() {
-  return <>
+  const { data: blogPosts } = api.blogPosts.getAll.useQuery()
+
+  return (
+    <>
       <Head>
         <title>chrsolr.me - blog</title>
       </Head>
-      <PageWrapper>
-        BLOG POST HERE
+      <PageWrapper className="pb-8">
+        {blogPosts?.map((post) => (
+          <div key={getUniqueKey()} className="mb-8">
+            <div className="w-fit">
+              <Link href={`/blog/${post.slug}`}>
+                <img
+                  src={post.coverImageUrl}
+                  alt={''}
+                  className="w-screen rounded-2xl"
+                />
+              </Link>
+            </div>
+            <Link href={`/blog/${post.slug}`} className="text-accent">
+              <Typography size="4xl" className="my-8">
+                {post.title}
+              </Typography>
+            </Link>
+
+            <Typography>{post.summary}</Typography>
+
+            <Link href={`/blog/${post.slug}`} className="text-accent">
+              <Typography>read more...</Typography>
+            </Link>
+          </div>
+        ))}
       </PageWrapper>
-  </>
+    </>
+  )
 }
