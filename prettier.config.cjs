f1ee43bcd85dbd54ca54dc16735ec411e65bf983
chrsolr <chr.solr@gmail.com>
2023-10-22 22:12:35 -0500

    Reset

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  semi: false,
-  trailingComma: "all",
-  singleQuote: true,
-}
-
-module.exports = config
--- src/components/ProgressBarDivider.tsx
+++
@@ -1,7 +0,0 @@
-export const ProgressBarDivider = () => {
-  return (
-    <div className="mb-4 h-2 w-full overflow-hidden rounded-md bg-[#a21f44]">
-      <div className="h-2 w-3/4 rounded-md bg-accent"></div>
-    </div>
-  )
-}
