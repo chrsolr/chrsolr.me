9998e4f3324ba37d7df29d87a1e5c47d9955ffac
chrsolr <chr.solr@gmail.com>
2023-10-29 01:13:13 -0500

    wip on about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,34 +1,48 @@
+import Image from 'next/image'
 import { PageWrapper } from '~/components/PageWrapper'
 import { Typography } from '~/components/Typography'
 
 export default function Page() {
   return (
     <PageWrapper>
-      <section>
-        <Typography size="2xl" weight="semibold">
-          <span className="text-accent">Christian </span>
-          <span className="text-accent">Soler</span>
+      <section className="flex flex-col">
+        <div className="flex flex-1 flex-col items-center">
+          <Image
+            width={100}
+            height={100}
+            alt="Avatar"
+            className="rounded-full"
+            src="/assets/images/1516804819724.jpeg"
+          />
+
+          <div className="ml-4 flex">
+            <span className="text-4xl font-light lowercase text-accent">
+              Christian{' '}
+            </span>
+            <span className="text-4xl font-semibold lowercase text-accent">
+              Soler
+            </span>
+          </div>
+
+          <Typography size="lg" className="lowercase text-gray-500">
+            Software Developer
+          </Typography>
+        </div>
+
+        <Typography size="xl" weight="semibold" className="text-accent">
+          About Me
         </Typography>
 
         <Typography>
-          As a passionate person, I pride myself in my work and every project I
-          take on. I am self-motivated and driven by puzzles and challenges.
-          These skills are what help me stand out as a developer. I enjoy
-          working in a team environment and thrive under pressure when working
-          on multiple projects.
-        </Typography>
-
-        <Typography>
-          I am a Full Stack Developer with 6+ years of hands-on experience
-          designing, developing, and implementing applications and solutions
-          using a range of technologies and programming languages. I am looking
-          for an opportunity in a company with a great environment. A place
-          where I can expand my knowledge and utilize my experience, as well as
-          become part of a team that is as passionate about programming as I am.
+          I am a dedicated Full Stack Developer with over six years of extensive
+          experience in conceiving, designing, and executing applications and
+          solutions using a diverse array of programming languages and
+          technologies. I take great pride in my work and am known for my
+          unwavering self-motivation, adept problem-solving abilities, and a
+          strong inclination towards thriving in collaborative, high-pressure
+          project environments.
         </Typography>
       </section>
-
-      <br />
 
       <section>
         <Typography size="xl" weight="semibold" className="text-accent">
@@ -44,17 +58,16 @@ export default function Page() {
         </Typography>
 
         <Typography>
-          Talk about the purpose of the site and the technogy behind it
+          Chrsolr.me have been residing on the internet since 2015, and here are
+          some particulars about its present form. It has been constructed using
+          the following frameworks and tools
         </Typography>
 
-        <ul>
-          <li>- NextJs 13 (App Router)</li>
-          <li>- Tailwind</li>
-          <li>- Typescript</li>
-          <li>- Neovim</li>
+        <ul className="mb-4 list-inside list-disc indent-4">
+          <li>Meticulously crafted with Kitty and Neovim.</li>
+          <li>Developed on NextJs 13, employing App Router and Typescript.</li>
+          <li>Personally customized with an in-house theme using Tailwind.</li>
         </ul>
-
-        <Typography>{''}</Typography>
       </section>
 
       <section>
