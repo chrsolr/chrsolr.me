a2e77e0c3f898745d499fcf88e5fba693e094ee1
Christian Soler <chr.solr@gmail.com>
2023-12-02 15:40:55 -0600

    Update page.tsx

--- src/app/aoc/page.tsx
+++ src/app/aoc/page.tsx
@@ -7,7 +7,7 @@ import Title from '~/components/Title'
 import Link from 'next/link'
 
 export default function Page() {
-  const aoc: Record<'2015' | '2022', AdventOfCodeTableItem[]> = {
+  const aoc: Record<'2015' | '2022' | '2023', AdventOfCodeTableItem[]> = {
     '2015': [
       {
         year: 2015,
