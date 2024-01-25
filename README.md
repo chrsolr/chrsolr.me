3e62bb08ccdb20e49571ac48c2cba8b3ba027806
chrsolr <chr.solr@gmail.com>
2024-01-25 08:11:12 -0600

    Reset. Starting Dotnet Implementations

--- README.md
+++
@@ -1,6 +0,0 @@
-# chrsolr.me
-
-### Todo's
-
-- Integration with Web API (Dotnet Core) or GoLang
-- Apps Page (Random stupid stuff)
--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
-const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  semi: false,
-  trailingComma: 'all',
-  singleQuote: true,
-}
-
-export default config
