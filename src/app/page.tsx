bd1e885e43304e170234d62a4b8d4afc8e06913d
chrsolr <chr.solr@gmail.com>
2024-07-21 17:27:35 -0500

    Updated Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { SubTitle } from "@/components/SubTitle";
 import { Typography } from "@/components/Typography";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Typography as="h1">chrsolr.me</Typography>
-      <Typography>hey!!!</Typography>
+      <Link href="https://github.com/chrsolr/">
+        <Typography as="h1">chrsolr.me</Typography>
+      </Link>
     </main>
   );
 }
