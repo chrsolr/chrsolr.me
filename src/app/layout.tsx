38f992063aba8536ce39befa7a757f99aa138103
iwchrsolr <christian@innowatts.com>
2024-07-22 16:04:35 -0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import { JetBrains_Mono } from "next/font/google";
-import "@/styles/globals.css";
-import { cn } from "@/lib/utils";
+import type { Metadata } from 'next'
+import { JetBrains_Mono } from 'next/font/google'
+import '@/styles/globals.css'
+import { cn } from '@/lib/utils'
 
 const mainFont = JetBrains_Mono({
-  weight: ["200", "400", "600"],
-  subsets: ["latin"],
-  variable: "--font-main",
-  display: "swap",
-});
+  weight: ['200', '400', '600'],
+  subsets: ['latin'],
+  variable: '--font-main',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
-  title: "chrsolr.me",
-  description: "Personal website",
-};
+  title: 'chrsolr.me',
+  description: 'Personal website',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="overflow-y-hidden dark" suppressHydrationWarning>
+    <html lang="en" className="dark overflow-y-hidden" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background text-foreground font-sans antialiased",
+          'min-h-screen bg-background font-sans text-foreground antialiased',
           mainFont.variable,
         )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,74 +1,74 @@
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
 export function Typography({
-  as = "p",
-  className = "",
+  as = 'p',
+  className = '',
   children,
 }: {
-  as?: "h1" | "h2" | "h3" | "h4" | "p" | "span";
-  className?: string;
-  children: ReactNode;
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'span'
+  className?: string
+  children: ReactNode
 }) {
-  if (as === "h1") {
+  if (as === 'h1') {
     return (
       <h1
         className={cn(
-          "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+          'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
           className,
         )}
       >
         {children}
       </h1>
-    );
+    )
   }
 
-  if (as === "h2") {
+  if (as === 'h2') {
     return (
       <h2
         className={cn(
-          "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+          'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
           className,
         )}
       >
         {children}
       </h2>
-    );
+    )
   }
 
-  if (as === "h3") {
+  if (as === 'h3') {
     return (
       <h3
         className={cn(
-          "scroll-m-20 text-2xl font-semibold tracking-tight",
+          'scroll-m-20 text-2xl font-semibold tracking-tight',
           className,
         )}
       >
         {children}
       </h3>
-    );
+    )
   }
 
-  if (as === "h4") {
+  if (as === 'h4') {
     return (
       <h4
         className={cn(
-          "scroll-m-20 text-xl font-semibold tracking-tight",
+          'scroll-m-20 text-xl font-semibold tracking-tight',
           className,
         )}
       >
         {children}
       </h4>
-    );
+    )
   }
 
-  if (as === "span") {
-    return <span className={cn("leading-4", className)}>{children}</span>;
+  if (as === 'span') {
+    return <span className={cn('leading-4', className)}>{children}</span>
   }
 
   return (
-    <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}>
+    <p className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}>
       {children}
     </p>
-  );
+  )
 }
