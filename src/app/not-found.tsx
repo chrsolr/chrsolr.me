1dea0173fa31b6170301dd70ddac996f40e35c9e
chrsolr <chr.solr@gmail.com>
2024-07-24 21:03:57 -0500

    Added 404 text to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,41 @@
 import { Typography } from '@/components/Typography'
+import { cn, getUniqueKey } from '@/lib/utils'
+
+function createText(content: Record<string, string>[]) {
+  return (
+    <div className="flex">
+      {content.map(({ text, color }) => {
+        return (
+          <Typography
+            key={getUniqueKey()}
+            className={cn('select-none', color)}
+            as="h1"
+          >
+            {text}
+          </Typography>
+        )
+      })}
+    </div>
+  )
+}
 
 export default function NotFound() {
   return (
-    <div className="flex container min-h-screen items-center justify-center min-w-full flex-col">
-      <div className="flex">
-        <Typography className="text-accent-yellow" as="h1">
-          Not
-        </Typography>
-        <Typography className="text-accent-blue" as="h1">
-          Found
-        </Typography>
-      </div>
+    <div className="container flex min-h-screen min-w-full flex-col items-center justify-center">
+      {createText([
+        { text: '4', color: 'text-accent-blue' },
+        { text: '0', color: 'text-accent-yellow' },
+        { text: '4', color: 'text-accent-pink' },
+      ])}
 
-      <Typography className="text-foreground">
+      {createText([
+        { text: 'Not', color: 'text-accent-yellow' },
+        { text: 'Found', color: 'text-accent-blue' },
+      ])}
+
+      <Typography className="mt-0 text-foreground-muted">
         Could not find requested resource
       </Typography>
-
-      <a className="text-accent-pink" href="/">
-        <Typography as="h3">Return Home</Typography>
-      </a>
     </div>
   )
 }
