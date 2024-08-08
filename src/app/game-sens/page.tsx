2ec112fc20e5e99a4f59f15c47a8cf7481ef71db
iwchrsolr <christian@innowatts.com>
2024-08-08 00:23:00 -0500

    Updated Game Sens Values

--- src/app/game-sens/page.tsx
+++ src/app/game-sens/page.tsx
@@ -10,15 +10,15 @@ import {
 } from '@/components/ui/table'
 
 export default function Page() {
-  const sensativities = [
+  const sensitivities = [
     {
       id: 'sens-1',
       game: 'Apex Legends',
       inputType: 'Mouse',
-      hipSens: '1.33',
+      hipSens: '1.3',
       adsSens: '1.0',
       fov: '110',
-      cm360: '39.06',
+      cm360: '39.97',
     },
     {
       id: 'sens-4',
@@ -33,19 +33,19 @@ export default function Page() {
       id: 'sens-2',
       game: 'Escape from Tarkov',
       inputType: 'Mouse',
-      hipSens: '0.23',
+      hipSens: '0.229',
       adsSens: '0.15',
       fov: '75',
-      cm360: '39.06',
+      cm360: '39.97',
     },
     {
       id: 'sens-3',
       game: 'Fortnite',
       inputType: 'Mouse',
-      hipSens: '5.267',
+      hipSens: '5.149',
       adsSens: '1',
       fov: 'N/A',
-      cm360: '39.06',
+      cm360: '39.97',
     },
   ]
 
@@ -53,7 +53,7 @@ export default function Page() {
     <div className="container pt-4">
       <div>
         <Typography as="h1" className="mb-4">
-          Gaming Sensativities
+          Gaming Sensitivities
         </Typography>
 
         <Badge className="mb-2 bg-accent-pink">Windows: 7</Badge>
@@ -63,42 +63,48 @@ export default function Page() {
         <Badge className="mb-4 bg-accent-purple">DPI: 800</Badge>
       </div>
 
-      <Table className="border">
-        <TableHeader>
-          <TableRow className="bg-background-light-accent">
-            <TableHead className="border-r text-center">Game</TableHead>
-            <TableHead className="border-r text-center">Input Type</TableHead>
-            <TableHead className="border-r text-center">Hip Sens</TableHead>
-            <TableHead className="border-r text-center">ADS Sens</TableHead>
-            <TableHead className="border-r text-center">cm/360</TableHead>
-            <TableHead className="border-r text-center">FOV</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {sensativities.map((sens) => (
-            <TableRow key={sens.id} className="bg-background-light">
-              <TableCell className="border-r">{sens.game}</TableCell>
-              <TableCell className="border-r text-center">
-                {sens.inputType === 'Mouse' ? (
-                  <Badge className="bg-accent-pink">Mouse</Badge>
-                ) : (
-                  <Badge className="bg-accent-purple">Controller</Badge>
-                )}
-              </TableCell>
-              <TableCell className="border-r text-right">
-                {sens.hipSens}
-              </TableCell>
-              <TableCell className="border-r text-right">
-                {sens.adsSens}
-              </TableCell>
-              <TableCell className="border-r text-right">
-                {sens.cm360}
-              </TableCell>
-              <TableCell className="border-r text-right">{sens.fov}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="rounded-md border">
+        <div className="relative w-full overflow-auto">
+          <Table className="w-full caption-bottom text-sm">
+            <TableHeader>
+              <TableRow className="bg-background-light-accent">
+                <TableHead className="text-center">Game</TableHead>
+                <TableHead className="text-center">Input Type</TableHead>
+                <TableHead className="text-center">Hip Sens</TableHead>
+                <TableHead className="text-center">ADS Sens</TableHead>
+                <TableHead className="text-center">cm/360</TableHead>
+                <TableHead className="text-center">FOV</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {sensitivities.map((sens) => (
+                <TableRow key={sens.id} className="bg-background-light">
+                  <TableCell className="border-r">{sens.game}</TableCell>
+                  <TableCell className="border-r text-center">
+                    {sens.inputType === 'Mouse' ? (
+                      <Badge className="bg-accent-pink">Mouse</Badge>
+                    ) : (
+                      <Badge className="bg-accent-purple">Controller</Badge>
+                    )}
+                  </TableCell>
+                  <TableCell className="border-r text-right">
+                    {sens.hipSens}
+                  </TableCell>
+                  <TableCell className="border-r text-right">
+                    {sens.adsSens}
+                  </TableCell>
+                  <TableCell className="border-r text-right">
+                    {sens.cm360}
+                  </TableCell>
+                  <TableCell className="border-r text-right">
+                    {sens.fov}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </div>
   )
 }
